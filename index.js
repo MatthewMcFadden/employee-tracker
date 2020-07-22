2fae1e124e3a7b5131a28538fd127856e264db85
Matt M <matt.mcfadden14@gmail.com>
2020-07-21 20:56:07 -0600

    First commit, Laid out index.js sections

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require("inquirer");
+
+// Creating Connection to Database
+
+// Introduction
+
+// View All Employees
+
+// View By Department
+
+// View By Role
+
+// Add New Employee
+
+// Update Employee Role
