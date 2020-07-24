10faf56dcf45f4adced2436d7cb9c0d68fbb5654
Matt M <matt.mcfadden14@gmail.com>
2020-07-23 19:39:02 -0600

    initialized node and inquirer. Added introduction function.

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 const inquirer = require("inquirer");
 
-// Creating Connection to Database
+// Connection to Database
 
 // Introduction
+function Introduction() {
+  inquirer.prompt({
+    name: "begin",
+    type: "list",
+    message: "What would you like to do?",
+    choices:
+      [
+        "View All Employees",
+        "View Employees by Department",
+        "View Employees by Role",
+        "Add Employee",
+        "Update Employee's Role",
+        "Finish"
+      ]
+  })
+  .then(function (answer) {
+    if (answer.begin === "View All Employees") {
+      viewAllEmployees();
+    }
+    else if (answer.begin === "View Employees by Department") {
+      viewByDepartment();
+    }
+    else if (answer.begin === "View Employees by Role") {
+      viewByRole();
+    }
+    else if (answer.begin === "Add Employee") {
+      addEmployee();
+    }
+    else if (answer.begin === "Update Employee's Role") {
+      updateEmployeeRole();
+    }
+    else if (answer.begin === "Finish") {
+      console.log("---> All Finished <---");
+    }
+    else {
+      connection.end();
+    }
+  })
+}
 
 // View All Employees
+viewAllEmployees();
 
 // View By Department
+viewByDepartment();
 
 // View By Role
+viewByRole();
 
 // Add New Employee
+addEmployee();
 
 // Update Employee Role
+updateEmployeeRole();
